0a7d7a3001b20038d68b2c952a8e55b53b5ecb20
Christoph Zilian <czilian@hotmail.com>
2017-11-14 15:52:00 +0800

    VBT logo

--- html/plugins/Test/Test.inc.php
+++ html/plugins/Test/Test.inc.php
@@ -1,3 +1,5 @@
 <?php
 
 echo 'Well done, the plugin system is up and running';
+echo '<iframe src="http://localhost" height="600" width="800"></iframe>';
+echo 'http://bonn.dcn.local:5601/goto/040942887c224a97f041dac7bbc74503';
